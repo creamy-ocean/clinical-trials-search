c66a287913a69a679fa00715a5254fcf26def4b4
Hyeyoung Yun <hyeyoungyun1214@gmail.com>
2023-09-08 23:55:42 +0900

    [Chore] 배포 환경 설정

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const config = {
-  baseURL: process.env.REACT_APP_AXIOS_BASE_URL,
+  baseURL: "http://localhost:4000",
   headers: { "Content-Type": "application/json" },
 };
 
